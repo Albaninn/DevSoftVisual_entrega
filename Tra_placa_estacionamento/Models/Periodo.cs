a5be839baa20a627ba0c68763646212e5c9fba9b
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-09-22 00:06:38 -0300

    Update Periodo.cs

--- Tra_placa_estacionamento/Models/Periodo.cs
+++ Tra_placa_estacionamento/Models/Periodo.cs
@@ -4,38 +4,15 @@ internal class Periodo
 {
     [Key]
 
-    private int? _idPeriodo;
-    private DateTime? _HoraEntrada;
-    private DateTime? _HoraSaida;
-
-
-
-    public int? IdPeriodo
-    {
-
-        get=>_idPeriodo;
-        set=>_idPeriodo = 0;
-
-
-    }
-
-    public DateTime? HoraEntrada
-    {
-        get=>_HoraEntrada;
-        set=>_HoraEntrada = value;
-    }
-
-    public DateTime? HoraSaida
-    {
-        get=> _HoraSaida;
-        set=> _HoraSaida = value;
-    }
+    public int? Id {get; set;}
+    public DateTime? _HoraEntrada {get; set;}
+    public DateTime? _HoraSaida {get; set;}
 
 
 
     public Periodo()
     {
-        _idPeriodo = new int();
+        id = new int();
         _HoraEntrada = new DateTime();
         _HoraSaida = null;
     }
