76cfac342d504e23f4e99a315b48c3e507f54693
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-09-22 00:05:24 -0300

    Update Moto.cs

--- Tra_placa_estacionamento/Models/Moto.cs
+++ Tra_placa_estacionamento/Models/Moto.cs
@@ -3,29 +3,16 @@ using System.ComponentModel.DataAnnotations;
 public class Moto : Veiculo
 {
     [Key]
-    private int? _IdMoto;
-    private Boolean? _Bau;
+    public int? Id {get; set;}
+    public Boolean? _Bau {get; set;}
 
     private Veiculo _veiculo;
 
-    public int? IdMoto
-    {
-        get=>_IdMoto;
-        set=>_IdMoto = value;
-    }
-
-    public Boolean? Bau
-    {
-        get=>_Bau;
-        set=>_Bau = value;
-    }
-
-
-
+   
     public Moto()
     {
         _Bau = false;
-        _IdMoto = null;
+         Id = null;
         _veiculo = new();
     }
 
