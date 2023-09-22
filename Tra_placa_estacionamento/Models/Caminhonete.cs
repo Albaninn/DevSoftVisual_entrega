be480a8376d87756a9d2dfb76b891fe9ae694f18
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-09-21 23:58:50 -0300

    Update Caminhonete.cs

--- Tra_placa_estacionamento/Models/Caminhonete.cs
+++ Tra_placa_estacionamento/Models/Caminhonete.cs
@@ -4,37 +4,17 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 public class Caminhonete : Veiculo
 {
     [Key]
-    private int? _IdCaminhonete ;
-    private int? _NroPortas;
-    private string? _Combustivel;
+    public int? Id {get; set;}
+    public int? _NroPortas {get; set;};
+    public string? _Combustivel {get; set;};
 
-    private Veiculo _veiculo;
+    public Veiculo _veiculo;
 
-
-    public int? id
-    {
-        get=> _IdCaminhonete;
-        set=> _IdCaminhonete = 0;
-
-    }
-
-    public int? NroPortas
-    {
-        get => _NroPortas;
-        set => _NroPortas = value;
-    }
-
-    public string? Combustivel
-    {
-        get=> _Combustivel;
-        set=> _Combustivel = value;
-
-    }
 
 
     public Caminhonete()
     {   
-        _IdCaminhonete = null;
+        Id = null;
         _NroPortas = null;
         _Combustivel = string.Empty;
         _veiculo = new();
