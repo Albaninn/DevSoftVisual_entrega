cb0e4b48581701e41f8ea40a7c1cef550bde49ef
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-09-22 00:01:42 -0300

    Update Cliente.cs

--- Tra_placa_estacionamento/Models/Cliente.cs
+++ Tra_placa_estacionamento/Models/Cliente.cs
@@ -3,43 +3,16 @@ using System.ComponentModel.DataAnnotations;
 public class Cliente
 {
     [Key]
-    private int? _IdCliente;
-    private string? _Nome;
-    private string? _Cpf;
-    private string? _Email;
+    public int? Id {get; set;};
+    public string? _Nome {get; set;};
+    public string? _Cpf {get; set;};
+    public string? _Email {get; set;};
 
-    /*public void SolicitaTicket {get; set;}*/
-
-
-    public int? IdCliente
-    {
-        get=> _IdCliente;
-        set=> _IdCliente = value;
-    }
-
-    public string? Nome
-    {
-        get=>_Nome;
-        set=> _Nome = value;
-    }
-
-    public string? Cpf
-    {
-        get=>_Cpf;
-        set=> _Cpf = value;
-    }
-
-    public string? Email
-    {
-        get=> _Email;
-        set=>_Email = value;
-    }
-
-
+    
 
     public Cliente()
     {
-        _IdCliente = null;
+        Id = null;
         _Cpf = string.Empty;
         _Nome = string.Empty;
         _Email = string.Empty;
