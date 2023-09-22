63bf9d5a87e67ac9ea7c78d2847d3332759c184c
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-09-22 00:13:08 -0300

    Update Veiculo.cs

--- Tra_placa_estacionamento/Models/Veiculo.cs
+++ Tra_placa_estacionamento/Models/Veiculo.cs
@@ -3,45 +3,18 @@ using System.ComponentModel.DataAnnotations;
 public class Veiculo
 {
     [Key]
-    private string? _Placa;
-    private int? _IdVeiculo;
-    private string? _Descricao;
-    public enum Cor {Vermelho, Branco, Preto, Prata};
+    public int? Id {get; set;}
+    public string? placa {get; set;}
+    public string? _Descricao {get; set;}
+    public enum Cor {Vermelho, Branco, Preto, Prata} {get; set;}
 
     private Cor _CorExterna;
     private Modelo? _Modelo;
 
 
-    public Cor CorExterna
-    {
-        get=>_CorExterna;
-        set=>_CorExterna = value;
-    }
-
-
-    public string? Placa
-    {
-        get => _Placa;
-        set => _Placa = value;
-    }
-
-    public int? IdVeiculo
-    {
-        get => _IdVeiculo;
-        set => _IdVeiculo = 0;
-    }
-
-    public string? Descricao
-    {
-        get=> _Descricao;
-        set=> _Descricao = value;
-
-    }
-
-    
     public Veiculo()
     {
-        _IdVeiculo= null;
+        Id = null;
         _Modelo = new();
         _Placa = string.Empty;
         _CorExterna = Cor.Branco;
