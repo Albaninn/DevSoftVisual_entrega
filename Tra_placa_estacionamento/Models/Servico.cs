14ec56cd6ee49ae4f17cdb463eb8118f28ca1136
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-09-22 00:08:33 -0300

    Update Servico.cs

--- Tra_placa_estacionamento/Models/Servico.cs
+++ Tra_placa_estacionamento/Models/Servico.cs
@@ -1,47 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 public class Servico
 {
-	[Key]
-	private int? _idServico;
-	private double _ValorPagar;
-	private string? _DescricaoServico;
-    private double _ValorHora;
+[Key]
+public int? Id {get; set;}
+public double _ValorPagar {get; set;}
+public string? _DescricaoServico {get; set;}
+public double _ValorHora {get; set;}
 
-    private Periodo _Periodo;//usar para retornar os horários de permanencia e multiplicar pelo valorHora;
-
-
-
-	public int? IdServico
-	{
-		get=> _idServico;
-		set=> _idServico = value;
-	}
-	public double ValorPagar
-	{
-		get=> _ValorPagar;
-		set=> _ValorPagar= value;
-	}
-
-	public string? DescricaoServico
-	{
-		get=> _DescricaoServico;
-		set=> _DescricaoServico = value;
-	}
-
-    public double? ValorHora
-    {
-        get => _ValorHora;
-        set => _ValorHora= 5.50;
-
-    }
+private Periodo _Periodo;//usar para retornar os horários de permanencia e multiplicar pelo valorHora;
 
 
-    public Servico()
-	{
-		_idServico = new int();
-		_ValorPagar = 0.0;
-		_DescricaoServico = string.Empty;
-	}
+
+
+public Servico()
+{
+	Id = new int();
+	_ValorPagar = 0.0;
+	_DescricaoServico = string.Empty;
+}
 
 
 
