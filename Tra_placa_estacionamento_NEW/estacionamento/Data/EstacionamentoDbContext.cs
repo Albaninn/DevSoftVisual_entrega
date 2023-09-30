04df42eb1df73dcf59fffdd2c67963d282ebd06c
Albaninn <lukalbanorb@gmail.com>
2023-09-30 17:17:39 -0300

    30.09
    Ajustado com todas as classes dentro do EstacionamentoDbContext

--- Tra_placa_estacionamento_NEW/estacionamento/Data/EstacionamentoDbContext.cs
+++ Tra_placa_estacionamento_NEW/estacionamento/Data/EstacionamentoDbContext.cs
@@ -8,6 +8,10 @@ public class EstacionamentoDbContext : DbContext
     public DbSet<Caminhonete>? caminhonete {get; set;}
     public DbSet<Cliente>? cliente {get; set;}
     public DbSet<NotaFiscal>? notasFiscais {get; set;}
+    public DbSet<Servico>? servico {get; set;}
+    public DbSet<Periodo>? periodo {get; set;}
+    public DbSet<Ticket>? ticket {get; set;}
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(connectionString: "DataSource=estacionamento.db;Cache=Shared");
