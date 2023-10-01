6fdd8abfaf07107f5c16a8cbcc2fe1752b29e431
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-10-01 13:57:58 -0300

    Add files via upload

---
+++ Tra_placa_estacionamento_NEW/estacionamento/Controllers/ServicoController.cs
@@ -0,0 +1,90 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tra_placa_estacionamento.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ServicoController : ControllerBase
+    {
+
+        //--------------------------------------------------------------------//
+
+        //injeção de dependencia para a acesso ao banco de dados sqlite;
+
+        private EstacionamentoDbContext _context;
+        public ServicoController(EstacionamentoDbContext context)
+        {
+            _context = context;
+        }
+
+        //--------------------------------------------------------------------//
+
+
+
+        [HttpGet]
+        [Route("listar")]
+        public async Task<ActionResult<IEnumerable<Servico>>> GetListarAsync()
+
+        {
+            if (_context.servico is null) return NotFound();
+            return await _context.servico.ToListAsync();
+
+        }
+
+
+
+        [HttpGet()]
+        [Route("buscar/{descricao}")]
+        public async Task<ActionResult<Servico>> Buscar([FromRoute] string descricao)
+        {
+            if (_context.servico is null) return NotFound();
+            var servico = await _context.servico.FindAsync(descricao);
+            return servico;
+        }
+
+        //--------------------------------------------------------------------//
+
+
+        [HttpPost()]
+        [Route("cadastrar")]
+        public async Task<IActionResult> Cadastrar(Servico servico)
+        {
+            await _context.AddAsync(servico);
+            await _context.SaveChangesAsync();
+            return Created("", servico);
+        }
+
+        //--------------------------------------------------------------------//
+
+        [HttpPut]
+        [Route("alterar")]
+        public async Task<IActionResult> Alterar(Servico servico)
+        {
+            _context.servico.Update(servico);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        //--------------------------------------------------------------------//
+        [HttpDelete]
+        [Route("excluir/{servico}")]
+        public async Task<IActionResult> excluir(int idservico)
+        {
+            var servico = await _context.servico.FindAsync(idservico);
+            if (_context.servico is null) return NotFound();
+            _context.servico.Remove(servico);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        //--------------------------------------------------------------------//
+
+
+
+
+    }
+
---
+++ Tra_placa_estacionamento_NEW/estacionamento/Controllers/TicketController.cs
@@ -0,0 +1,98 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Tra_placa_estacionamento.Controllers;
+
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketController : ControllerBase
+    {
+
+            //--------------------------------------------------------------------//
+
+            //injeção de dependencia para a acesso ao banco de dados sqlite;
+
+            private EstacionamentoDbContext _context;
+            public TicketController(EstacionamentoDbContext context)
+            {
+                _context = context;
+            }
+
+            //--------------------------------------------------------------------//
+
+
+
+
+            [HttpGet()]
+            [Route("listarticket")]
+            public async Task<ActionResult<IEnumerable<Ticket>>> GetListarAsync()
+            {
+                if (_context.ticket is null) return NotFound();
+                return await _context.ticket.ToListAsync();
+            }
+
+
+            //--------------------------------------------------------------------//
+
+            [HttpGet()]
+            [Route("buscar/{COD}")]
+            public async Task<ActionResult<Ticket>> Buscar([FromRoute] int codTicket)
+            {
+                if (_context.ticket is null) return NotFound();
+                var ticket = await _context.ticket.FindAsync(codTicket);
+                return ticket;
+            }
+
+
+            //--------------------------------------------------------------------//
+
+            [HttpPost()]
+            [Route("inserir")]
+            public async Task<ActionResult<Ticket>> Inserir([FromBody] Ticket ticket)
+            {
+                _context.ticket.Add(ticket);
+                await _context.SaveChangesAsync();
+                return ticket;
+            }
+
+            //--------------------------------------------------------------------//
+
+
+            [HttpPut]
+            [Route("alterar")]
+            public async Task<IActionResult> Alterar(Ticket ticket)
+            {
+                _context.ticket.Update(ticket);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            //--------------------------------------------------------------------//
+
+            [HttpDelete]
+            [Route("excluir/{COD}")]
+            public async Task<IActionResult> Excluir([FromRoute] string codTicket)
+            {
+                var ticket = await _context.ticket.FindAsync(codTicket);
+                if (ticket is null) return NotFound();
+                _context.ticket.Remove(ticket);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            //--------------------------------------------------------------------//
+
+
+
+
+
+            
+
+
+
+            
+        
+    }
+
