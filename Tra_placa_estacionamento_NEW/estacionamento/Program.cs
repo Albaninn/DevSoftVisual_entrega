c0626e70f2f498661c6dcb99928cdd63911b9544
Albaninn <lukalbanorb@gmail.com>
2023-10-02 19:44:44 -0300

    02.10
    add linha em program.cs para DBContext

--- Tra_placa_estacionamento_NEW/estacionamento/Program.cs
+++ Tra_placa_estacionamento_NEW/estacionamento/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<EstacionamentoDbContext>(); //Linha adicionada a mão
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
